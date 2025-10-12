74286e59cfc7350ff32251ff751a45c0ecdd26fe
stephen muiru <stevembugua26@gmail.com>
2025-10-12 14:58:40 +0300

    setting up components

---
+++ src/Home.jsx
@@ -0,0 +1,21 @@
+export function home() {
+  
+  return(
+    <body>
+    <ul>
+      <li>
+      </li>
+      <li>Home</li>
+      <li>Shop</li>
+      <li>Cart</li>
+    </ul>
+
+    <div className="home-content">
+      <h1>EVERYDAY ESSENTIALS BUILT TO HANDLE ANYTHING</h1>
+      <img src="/images/heber-davis-X_55Pa9E8h4-unsplash.jpg" alt="a man working" />
+    </div>
+    </body>
+
+    
+  )
+}
