49565645e77c00ea6435b739c8eedd6c8ed95246
stephen muiru <stevembugua26@gmail.com>
2025-10-21 17:29:27 +0300

    working on shop page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export function home() {
+export function Home() {
   
   return(
     <div>
--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import { useState } from 'react' 
 
-export function shop(){
+export function Shop(){
 
     const[cart, setCart] = useState([])
 
 
     const products = [
 
-        {id: 1, name: "Detroit style jacket", price: 126, image: "src/images/Carhartt exports/a135d43f87141338e967fbf723a74853.jpg" },
-        {id : 2, name: "Loose Sid Trousers", price: 80, image: 'src/images/Carhartt exports/0e1869609b94c79cbc74d2ef207185ec.jpg' },
-        {id : 3, name: "Relaxed fit shirt", price: 12, image: 'src/images/Carhartt exports/81144753f985618d03bba7084149a274.jpg' },
+        {id: 1, name: "Detroit style jacket", price: 126, image: "/images/Carhartt exports/a135d43f87141338e967fbf723a74853.jpg" },
+        {id : 2, name: "Loose Sid Trousers", price: 80, image: '/images/Carhartt exports/0e1869609b94c79cbc74d2ef207185ec.jpg' },
+        {id : 3, name: "Relaxed fit shirt", price: 12, image: '/images/Carhartt exports/81144753f985618d03bba7084149a274.jpg' },
         {id : 4, name: "Hamilton waterproof Boots", price: 190, image: '/images/Carhartt exports/530029e4c712b77ed7580e798d1b25de.jpg' },
-        {id : 5, name: "Relaxed fit duck overall", price: 280, image: 'src/images/Carhartt exports/88e8724b8cfa23109f2382a8772a5174.jpg' },
-        {id : 6, name: "Knit Embossed Beenie", price: 20, image: 'src/images/Carhartt exports/2c713939d19a578ac1f615e3c5e8ed42.jpg' }
+        {id : 5, name: "Relaxed fit duck overall", price: 280, image: '/images/Carhartt exports/88e8724b8cfa23109f2382a8772a5174.jpg' },
+        {id : 6, name: "Knit Embossed Beenie", price: 20, image: '/images/Carhartt exports/2c713939d19a578ac1f615e3c5e8ed42.jpg' }
     ]
 
     
 
 
     const handleClick = () => {
-      
+      setCart([...cart, product])
     }
 
     return (
@@ -29,72 +29,81 @@ export function shop(){
             
             <div className='product-display'>
             <div className='grid'>
-               <div className='card'>
-                <img src='src/images/Carhartt exports/a135d43f87141338e967fbf723a74853.jpg' className='product-img' alt='a jacket' />
-                <div className='product-details'>
-                    {products.map(product => (
-                      <h3 key={product.id}>Detroit style jacket</h3>  
-                    ))}
-                    <p className='price'>126$</p>
-                    <button type='button' onClick={handleClick}>Add to cart</button>
-                </div>
-                </div>
+                {products.map(product => (
+                  <div className='card' key={product.id}>
+                  <img src={product.image} className='product-img' alt='a jacket' />
+                  <div className='product-details'>
+                        <h3>{product.name}</h3>  
+                      <p className='price'>{product.price}</p>
+                      <button type='button' onClick={() => handleClick(product)}>Add to cart</button>
+                  </div>
+                  </div>
+                ))}
             </div>
 
             <div className='grid'>
-               <div className='card'>
-                <img src='src/images/Carhartt exports/0e1869609b94c79cbc74d2ef207185ec.jpg' className='product-img' alt='a sid trouser' />
+                <div className='card' key={product.id}>
+                <img src={product.image} className='product-img' alt='a sid trouser' />
                 <div className='product-details'>
-                    <h3>Loose Sid Trousers</h3>
-                    <p className='price'>80$</p>
-                    <button type='button'>Add to cart</button>
+                    <h3>{product.name}</h3>
+                    <p className='price'>{product.price}</p>
+                    <button type='button' onClick={() => handleClick(product)}>Add to cart</button>
                 </div>
                 </div> 
+               
             </div>
 
             <div className='grid'>
-               <div className='card'>
-                <img src='src/images/Carhartt exports/81144753f985618d03bba7084149a274.jpg' className='product-img' alt='a tshirt' />
-                <div className='product-details'>
-                    <h3>Relaxed fit shirt</h3>
-                    <p className='price'>12$</p>
-                    <button type='button'>Add to cart</button>
-                </div>
-                </div> 
+                
+                  <div className='card' key={product.id}>
+                  <img src={product.image} className='product-img' alt='a tshirt' />
+                  <div className='product-details'>
+                      <h3>{product.name}</h3>
+                      <p className='price'>{product.price}</p>
+                      <button type='button' onClick={() => handleClick(product)}>Add to cart</button>
+                  </div>
+                  </div> 
+               
             </div>
 
             <div className='grid'>
-               <div className='card'>
-                <img src='/images/Carhartt exports/530029e4c712b77ed7580e798d1b25de.jpg' className='product-img' alt='a pair of boots' />
-                <div className='product-details'>
-                    <h3>Hamilton waterproof Boots</h3>
-                    <p className='price'>190$</p>
-                    <button type='button'>Add to cart</button>
-                </div>
-                </div> 
+                   <div className='card' key={product.id}>
+                   <img src={product.image} className='product-img' alt='a pair of boots' />
+                   <div className='product-details'>
+                       <h3>{product.name}</h3>
+                       <p className='price'>{product.price}</p>
+                       <button type='button' onClick={() => handleClick(product)}>Add to cart</button>
+                   </div>
+                   </div> 
+              
             </div>
 
             <div className='grid'>
-               <div className='card'>
-                <img src='src/images/Carhartt exports/88e8724b8cfa23109f2382a8772a5174.jpg' className='product-img' alt='an overall' />
-                <div className='product-details'>
-                    <h3>Relaxed fit duck overall</h3>
-                    <p className='price'>280$</p>
-                    <button type='button'>Add to cart</button>
-                </div>
-                </div> 
+                
+                   <div className='card' key={product.id}>
+                   <img src={product.image} className='product-img' alt='an overall' />
+                   <div className='product-details'>
+                       <h3>{product.name}</h3>
+                       <p className='price'>{product.price}</p>
+                       <button type='button' onClick={() => handleClick(product)}>Add to cart</button>
+                   </div>
+                   </div> 
+              
             </div>
 
             <div className='grid'>
-               <div className='card'>
-                <img src='src/images/Carhartt exports/2c713939d19a578ac1f615e3c5e8ed42.jpg' className='product-img' alt='a beenie' />
-                <div className='product-details'>
-                    <h3>Knit Embossed Beenie</h3>
-                    <p className='price'>20$</p>
-                    <button type='button'>Add to cart</button>
-                </div>
-                </div> 
-            </div>
+                
+                  <div className='card' key={product.id}>
+                  <img src={product.image} className='product-img' alt='a beenie' />
+                  <div className='product-details'>
+                      <h3>{product.name}</h3>
+                      <p className='price'>{product.price}</p>
+                      <button type='button' onClick={() => handleClick(product)}>Add to cart</button>
+                  </div>
+                  </div> 
+        
+
+               </div>
             </div>
 
 
