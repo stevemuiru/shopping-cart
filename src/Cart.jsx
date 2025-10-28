bb3db648c2ca40b74a72e2c966749b1c4bdedb67
stephen muiru <stevembugua26@gmail.com>
2025-10-28 17:26:37 +0300

    fixing display on cart

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 
 export function Cart({cart}) {
-    <h2>Your Cart</h2>
- if(cart.length === '') {
-    return 'Cart is empty'
- } else {
-    const listItems = cart.map(product => {
-        <li>{image}</li>,
-        <li>{name }</li>,
-        <li>{price}</li>,
-        <li>{quantity}</li>
-    })
-    return <ul>listItems</ul>
- }
+   if(cart.length === 0){
+      return "Your cart is empty, continue shopping."
+   } else{
+      const listItems = cart.map((product, index) => {
+         return (
+            <ul>
+             <li key={index}>[product image], [product name], [price], [quantity] [remove button]</li>
+            </ul>  
+         )
+      })
+   }
 }
