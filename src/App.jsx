64066a9a2081c3acee80273a278cf72de1eed1d7
stephen muiru <stevembugua26@gmail.com>
2025-11-05 17:34:50 +0300

    finalizing touches

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,43 @@ function App() {
   const [cart, setCart] = useState([]);
 
   
+  const addToCart = (product) => {
+    setCart(prev => {
+      const exists = prev.find(p => p.id === product.id);
+      if (exists) {
+        return prev.map(p =>
+          p.id === product.id ? { ...p, quantity: p.quantity + 1 } : p
+        );
+      }
+      return [...prev, { ...product, quantity: 1 }];
+    })
+  }
+
+  const removeFromCart = (productId) => {
+    const updatedArray = cart
+      .map(item => {
+        if (item.id === productId) {
+          if (item.quantity > 1) {
+            return { ...item, quantity: item.quantity - 1 }
+          } else {
+            return null
+          }
+        }
+        return item
+      })
+      .filter(Boolean)
+  
+    setCart(updatedArray)
+  }
+  
+
+  
 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/shop" element={<Shop cart={cart} setCart={setCart} />} />
-      <Route path="/cart" element={<Cart cart={cart} setCart={setCart} />} />
+      <Route path="/shop" element={<Shop products={product} addToCart={addToCart} cart={cart} />} />
+      <Route path="/cart" element={<Cart cart={cart} removeFromCart={removeFromCart} />} />
     </Routes>
   );
 }
--- src/Shop.jsx
+++ src/Shop.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 import { Cart } from './Cart'
 import { useState } from 'react' 
 
-export function Shop({cart, setCart}){
+export function Shop({addToCart,cart}){
 
     
 
@@ -18,65 +18,32 @@ export function Shop({cart, setCart}){
         {id : 6, name: "Knit Embossed Beenie", price: 20, image: '/images/Carhartt exports/2c713939d19a578ac1f615e3c5e8ed42.jpg' }
     ]
 
-    
-
-
-    const handleClick = (clickedProduct) => {
-      const foundItem = cart.find(item => item.id === clickedProduct.id)
-      
-      if(foundItem) {
-        const updatedItems = cart.map(item => {
-            if(item.id === clickedProduct.id) {
-                return {...item, quantity: item.quantity + 1 } 
-            }
-            return item;
-        });
-        setCart(updatedItems)
-      } else { 
-        const newItem = {...clickedProduct, quantity : 1 }
-        setCart([...cart, newItem])
-      }
-    }
-
-   const removeFromCart = (productId) => {
-    const updatedArray = cart.map(item => {
-      if(item.id === productId) {
-        if(item.quantity > 1) {
-          return {...item, quantity: item - 1}
-        } else {
-          return null
-        }
-      }
-      return item
-    }).filter(Boolean) 
-    
-    setCart(updatedArray)
-   }
-
     return (
-        <div>
-          <h2>Our Shop</h2>
-          <div className='product-display'>
-            <div className='grid'>
-              {products.map(product => (
-                <div className='card' key={product.id}>
-                  <img src={product.image} className='product-img' alt={product.name} />
-                  <div className='product-details'>
-                    <h3>{product.name}</h3>
-                    <p className='price'>{product.price}$</p>
-                    <button type='button' onClick={() => handleClick(product)}>Add to cart</button>
-                  </div>
+      <div>
+        <h2>Our Shop</h2>
+    
+        <div className='product-display'>
+          <div className='grid'>
+            {products.map(product => (
+              <div className='card' key={product.id}>
+                <img src={product.image} className='product-img' alt={product.name} />
+                <div className='product-details'>
+                  <h3>{product.name}</h3>
+                  <p className='price'>{product.price}$</p>
+                  <button type='button' onClick={() => addToCart(product)}>Add to cart</button>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
-          
-          {cart.length > 0 && (
-           <div className="cart-link">
-            <Link to="/cart">Proceed to Cart →</Link>
-         </div>
-)}
         </div>
-      )
-} 
-
+    
+        {cart.length > 0 && (
+          <div className='cart-link'>
+            <Link to="/cart">Proceed to Cart →</Link>
+          </div>
+        )}
+      </div>
+    ) 
+    
+    }  
+    
